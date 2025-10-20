f0a87eb154641aa4864caf076c10fe74ac061ba9
m-uson <usonibrahimovic@gmail.com>
2025-10-20 22:39:18 +0600

    add sonner  shadcn

---
+++ src/components/providers.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { AuthUIProvider } from "@daveyplate/better-auth-ui";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import type { ReactNode } from "react";
+import { authClient } from "~/lib/auth-client";
+
+export function Providers({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
+  return (
+    <AuthUIProvider
+      authClient={authClient}
+      navigate={(...args) => router.push(...args)}
+      replace={(...args) => router.replace(...args)}
+      onSessionChange={() => {
+        router.refresh();
+      }}
+      Link={Link}
+    >
+      {children}
+    </AuthUIProvider>
+  );
+}
